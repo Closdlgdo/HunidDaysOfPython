2ac903ad89715f1cf8f9bb7100f54aa1a2142fb9
Closdlgdo <cidstp031921@gmail.com>
2023-10-10 06:25:58 -0700

    Starting loops with CS50. Learning while loops and for loops.

---
+++ sidequests/loops/lists_of_things.py
@@ -0,0 +1,12 @@
+# a for loop is used to iterate over a list of objects.
+
+for obj in [0, 1, 2]:
+    print("Carlos")
+
+# to make the top code usable with higher values, we need to add a new function called "range"
+# this function allows us to give a starting point and a stop point.
+for obj in range(33):
+    print("delgado")
+
+# another way to print this in the simplest way is:
+print("Carlos\n" * 3, end="")
---
+++ sidequests/loops/while_pets.py
@@ -0,0 +1,20 @@
+# starting with loops. My dogs are barking
+print("Woof")
+print("Woof")
+print("Woof")
+
+# While loops time. It goes from line 7 to line 8 to line 9. You need to add line 10 to stop the loop.
+i = 0
+while i < 3:
+    print("Woof")
+    i += 1  # i = i + 1
+
+i = 1
+while i <= 3:
+    print("Woof")
+    i += 1  # i = i + 1
+
+a = 3
+while a != 0:
+    print("Woof")
+    a -= 1  # i = i - 1
