dffe41e689bbaf1efed679ff96c2590ded46a7c4
Closdlgdo <cidstp031921@gmail.com>
2023-10-10 06:42:59 -0700

    created a function for barking and another function that receives user input and prints out the quantity of said input using a while loop and a for _ in range loop.

--- sidequests/loops/while_pets.py
+++ sidequests/loops/while_pets.py
@@ -20,25 +20,37 @@
 #     a -= 1  # i = i - 1
 
 # if we want to purposely induce an infinite loop, we can use this:
-while True:
-    en = int(input("How old is your pet? "))
-    if en < 0:
-        continue  # this is telling the code to continue the same loop until the user gives a proper input.
-    else:
-        break  # this will break us out of the loop if the input is not less than 0.
+# while True:
+#     en = int(input("How old is your pet? "))
+#     if en < 0:
+#         continue  # this is telling the code to continue the same loop until the user gives a proper input.
+#     else:
+#         break  # this will break us out of the loop if the input is not less than 0.
+#
+# # we can also use this without an if, else statement:
+# while True:
+#     en = int(input("How old is your pet? "))
+#     if en > 0:
+#         break
+# for _ in range(en):
+#     print("Woof")
+#
 
-# we can also use this without an if, else statement:
-while True:
-    en = int(input("How old is your pet? "))
-    if en > 0:
-        break
-for _ in range(en):
-    print("Woof")
-
-
-# time for a function for all this code.
+# This code defines a function called main which calls two other functions: get_number and woof.
+# The get_number function is expected to return a number, and that number is passed as an argument to the woof function.
 def main():
-    woof(3)
+    number = get_number()
+    woof(number)
+
+
+# This code defines a function called get_number that repeatedly asks the user for input until they enter a number
+# greater than 0. It then returns that number.
+def get_number():
+    while True:
+        en = int(input("What is en? "))
+        if en > 0:
+            break
+    return en
 
 
 # This code defines a function called woof that takes an argument n. It then prints the word "Woof" n times.
@@ -48,3 +60,4 @@ def woof(n):
 
 
 main()
+# Now we have the ability to take user input and print out the quantity they wanted.
