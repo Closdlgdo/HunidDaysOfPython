2274b616749cf66254e9689c354df4543434aa23
Closdlgdo <cidstp031921@gmail.com>
2023-10-14 06:51:40 -0700

    Finished up another lesson with CS50 about catching exceptions. Now learning about libraries like Random. Also, created a file with some problems to work on on the side.

---
+++ sidequests/HW/EasyHW.py
@@ -0,0 +1,62 @@
+# Exercise 1: Simple Math Operations
+# Write a Python program that:
+#
+# Takes two numbers as input from the user.
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+# Calculates and prints their sum, difference, product, and quotient.
+summation = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+
+print("Sum:", summation, "Difference:", difference, "Product:", product, "Quotient:", quotient)
+
+# Exercise 2: Factorial Calculator
+# Write a Python function that calculates the factorial of a given number. Test it by finding the factorial of 5.
+#
+#
+
+# Exercise 3: Guess the Number Game
+# Write a program that generates a random number between 1 and 100. The user should guess the number. Provide hints like
+# "Too high" or "Too low" until they guess correctly.
+#
+#
+
+# Exercise 4: Fibonacci Sequence
+# Write a Python function that generates the first n numbers in the Fibonacci sequence.
+#
+#
+
+# Exercise 5: String Reversal
+# Write a function that takes a string as input and returns the reverse of the string.
+#
+#
+
+# Exercise 6: Prime Number Checker
+# Write a function that checks if a given number is prime or not.
+#
+#
+
+# Exercise 7: List Manipulation
+# Write a program that:
+#
+# Creates a list of numbers.
+# Removes the duplicates.
+# Finds the maximum and minimum values.
+# Sorts the list in ascending order.
+#
+#
+
+# Exercise 8: Palindrome Checker
+# Write a function that checks if a given word or phrase is a palindrome.
+#
+#
+
+# Exercise 9: Rock, Paper, Scissors Game
+# Write a Python program that allows the user to play a game of Rock, Paper, Scissors against the computer.
+#
+#
+
+# Exercise 10: File Handling
+# Write a program that reads a text file, counts the frequency of each word, and prints the top 5 most frequent words.
---
+++ sidequests/libraries/libraries.py
@@ -0,0 +1,4 @@
+# Libraries are files of code that other have written and can be used with others.
+# Modules are libraries that have one or more functions.
+# Random library is a module in Python. It is used to generate random numbers.
+# Import is a module in Python. It is used to import other modules.
