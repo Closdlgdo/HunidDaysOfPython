b8e0c51835e58eff6c197aa33e9bc3d83a3c6544
Closdlgdo <cidstp031921@gmail.com>
2023-10-16 06:34:01 -0700

    Starting on Day8 for 100 DOP. Re practicing functions.

---
+++ Day8/funcsWinputs.py
@@ -0,0 +1,18 @@
+# Functions are a really handy way of taking a complex set of instructions and packaging them together inside a block
+# of code that has a name given to it.
+# def my_function():
+# Do this
+# Then Do that
+# Finally do this
+
+# # Review:
+# # Create a function called greet().
+# def greet():
+#     print("Hello")
+#     print("Welcome")
+#     print("To Python")
+# # Write 3 print statements inside the function.
+# # Call the greet() function and run your program.
+#
+#
+# greet()
