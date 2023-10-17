f18ff677284d00423a9d2f838949d113bd06e66d
Closdlgdo <cidstp031921@gmail.com>
2023-10-17 06:04:27 -0700

    Starting on Day8 for 100 DOP. Re practicing functions with inputs.

--- Day8/funcsWinputs.py
+++ Day8/funcsWinputs.py
@@ -1,9 +1,11 @@
 # Functions are a really handy way of taking a complex set of instructions and packaging them together inside a block
 # of code that has a name given to it.
+#
 # def my_function():
-# Do this
-# Then Do that
-# Finally do this
+#   Do this
+#   Then Do that
+#   Finally do this
+# my_function()
 
 # # Review:
 # # Create a function called greet().
@@ -16,3 +18,20 @@
 #
 #
 # greet()
+#
+#
+# The function with a return is when you put somthing in the parenthesis ().
+
+# def my_function(something):
+#   Do this with something
+#   Then Do that
+#   Finally do this
+# my_function(123)  # When you put in a value here (argument), that same value gets put into the top parenthesis (parameter),
+# after that, the value goes into the actual code to run.
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"Welcome to Python {name}")
+
+
+greet_with_name("Bambi")
