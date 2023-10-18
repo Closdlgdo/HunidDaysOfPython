0a0dae99f044013a8006fb69092cce6d52c40f63
Closdlgdo <cidstp031921@gmail.com>
2023-10-17 19:49:56 -0700

    Trying to practice a bit more with exception handling using another yt source. BroCode.

--- sidequests/exceptions/tryMeExcept.py
+++ sidequests/exceptions/tryMeExcept.py
@@ -70,24 +70,34 @@
 # Calls a function called get_int() which returns an integer value.
 # Stores the returned integer value in a variable called x.
 # Prints the value of x using f-string formatting.
-def main():
-    x = get_int("What's x? ")
-    print(f'x is: {x}')
-
-
-# This code defines a function called get_int() that repeatedly asks the user for input until they provide a valid integer.
-# It uses a while loop and a try-except block to handle any ValueError that may occur if the user enters a non-integer
-# value. Once a valid integer is provided, the function breaks out of the loop and returns the value.
-
-
-def get_int(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-            # return can break you out of a loop and return a value. Stronger and more useful thank Break.
-        except ValueError:
-            pass  # the pass statement is a placeholder that does nothing. It is used when a statement is syntactically
-            # required but you do not want to perform any action at that point in your code.
-
-
-main()
+# def main():
+#     x = get_int("What's x? ")
+#     print(f'x is: {x}')
+#
+#
+# # This code defines a function called get_int() that repeatedly asks the user for input until they provide a valid integer.
+# # It uses a while loop and a try-except block to handle any ValueError that may occur if the user enters a non-integer
+# # value. Once a valid integer is provided, the function breaks out of the loop and returns the value.
+#
+#
+# def get_int(prompt):
+#     while True:
+#         try:
+#             return int(input(prompt))
+#             # return can break you out of a loop and return a value. Stronger and more useful thank Break.
+#         except ValueError:
+#             pass  # the pass statement is a placeholder that does nothing. It is used when a statement is syntactically
+#             # required but you do not want to perform any action at that point in your code.
+#
+#
+# main()
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+    print(result)
+except ZeroDivisionError:  # it is not considered good practice to use a single Except block that would handle all exceptions.
+    # It is better practice to handle specific exceptions.
+    print("You can't divide by zero!")
+except ValueError:
+    print("Oops! That was not a valid number. Try again...")
