3f23463b378f088ec73f7044dbbbc87e5ed5d240
Closdlgdo <cidstp031921@gmail.com>
2023-10-20 21:30:38 -0700

    We are working with the sys module and the command line, this time we removed the exception handling and used conditionals (if, else) to better show the inputer what we need from them.

--- sidequests/commandLineArgs/practice.py
+++ sidequests/commandLineArgs/practice.py
@@ -4,10 +4,15 @@
 # This is useful to influence the behavior of your program based of their input.
 import sys
 
-try:
-    print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments.")
+# let's be a bit more defensive with the code and give a refined exception.
+if len(sys.argv) < 2:
+    print("Too few arguments!")
+elif len(sys.argv) > 2:
+    print("Too many arguments!")
+else:
+    print(f"Hello, my name is", sys.argv[1])
+# this way we don't have to give an exception, we can use the conditionals that could express that.
+
 # This is what I would input into the terminal to run the program:
 # python3 practice.py Carlos
 # It prints out "Hello, my name is Carlos"
