b594ece37b3937aab640b8902dab3a8785b97482
Closdlgdo <cidstp031921@gmail.com>
2023-10-22 06:41:03 -0700

    CS50 We are now wanting a specific section of the JSON response. and so we create a for loop that cycles through the output and gives us back what we want.

--- sidequests/packages/itunes.py
+++ sidequests/packages/itunes.py
@@ -2,7 +2,6 @@
 # We basically pretend to be a browser, connect to the api on a server and download some data that we can add to our page.
 # A popular package that pip has is called requests. The request library allows us to make HTTP/s requests.
 # JSON is a language agnostic format that is used to transfer data from one program to another.
-import json
 import sys
 
 import requests
@@ -11,7 +10,10 @@ if len(sys.argv) != 2:  # error checking; if len(sys.argv) is not equal to 2. we
     sys.exit("Too many arguments!")
 # request.get is a function inside the request package that allows us to make HTTP/s requests.
 # it gets stored in a variable called response.
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
 
-# We now integrate json.dump function and pretty formats the printing.
-print(json.dumps(response.json(), indent=2))
+# We will store the json response in a variable called o.
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
+    # Here we are interested in the track name only from the response.
