220c39c288f3ae4d97a6579752a1acf1a86a5e15
Closdlgdo <cidstp031921@gmail.com>
2023-10-26 05:28:14 -0700

    We replace the print statement with a return to return the formatted string. we then save the output to a variable and then print the variable.

--- Day10/first_name_last.py
+++ Day10/first_name_last.py
@@ -2,8 +2,11 @@
 def format_name(f_name, l_name):
     formatted_f_name = f_name.title()  # we are capturing the f_name.title() into a variable called formatted_f_name
     formatted_l_name = l_name.title()
-    print(f"{formatted_f_name} {formatted_l_name}")  # we then print out the formatted_f_name and formatted_l_name using
-    # f-string interpolation.
+
+    return f"{formatted_f_name} {formatted_l_name}"  # replacing the print function with return allows this part of the code
+    # to replace the call function
 
 
-format_name("carLoS", "dElGadO")
+# Since we are returning the formatted string as the output, we will save the output inside a variable.
+formatted_string = format_name("carLoS", "dElGadO")
+print(formatted_string)
