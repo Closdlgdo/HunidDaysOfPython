9ecf8ee52224c2493b1c3e4473715ee1ca4a463c
Closdlgdo <cidstp031921@gmail.com>
2023-10-26 05:15:48 -0700

    Day10 of 100DOP we are starting to learn and write our own functions with outputs.

---
+++ Day10/first_name_last.py
@@ -0,0 +1,7 @@
+# Time to create some functions with outputs!
+def format_name(f_name, l_name):
+    print(f_name.title())
+    print(l_name.title())
+
+
+format_name("carlos", "THEANO")
---
+++ Day10/funcs_with_outputs.py
@@ -0,0 +1,20 @@
+# Functions
+# def my_function():
+#   # Do this
+#   # Then do this
+#   # Finally do this
+# my_function()
+
+# Functions with Inputs
+# def my_function(something):
+#   # Do this with something
+#   # Then do this
+#   # Finally do this
+# my_function(123)
+
+# Functions with Outputs
+def my_function():
+    return 3 * 2
+
+
+print(my_function())
