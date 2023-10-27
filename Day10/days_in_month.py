a138fb735f57b8d5f6eae2e3dd080a3cf11ebf11
Closdlgdo <cidstp031921@gmail.com>
2023-10-27 05:23:16 -0700

    We created a program that can receive input of a certain year and month. It will return the amount of days that month had in that year.

---
+++ Day10/days_in_month.py
@@ -0,0 +1,25 @@
+# We will be creating a program that will return the quantity of days in a respective month.
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
+
+year = int(input("Year: "))
+month = int(input("Month: "))
+days = days_in_month(year, month)
+print(days)
