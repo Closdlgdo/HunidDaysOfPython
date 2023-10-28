23a04c3406158e9c629d0aeecb7707b041cd8706
Closdlgdo <cidstp031921@gmail.com>
2023-10-28 06:16:31 -0700

    CS50, we are creating our own library where we can use functions from one py file onto another py file. The problem is that main is getting called no matter what and is printing out unnecessary things.

--- sidequests/own_libraries/sayings.py
+++ sidequests/own_libraries/sayings.py
@@ -11,4 +11,5 @@ def goodbye(name):
     print(f"Goodbye {name}!")
 
 
-main()
+main()  # it seems that what we want to print int the say.py file is not properly printing because we are blindly
+# calling the function main(). Main() gets called no matter what.
