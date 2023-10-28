88475c6f31e5e9efa2186009b123303d1a03063f
Closdlgdo <cidstp031921@gmail.com>
2023-10-28 06:25:04 -0700

    CS50, we wrap the main function in a conditional that way the entire main() is not called when imported into the say.py file. Now this allows us to work with each function respectively.

--- sidequests/own_libraries/say.py
+++ sidequests/own_libraries/say.py
@@ -1,11 +1,16 @@
 import sys
 
-from sayings import hello
+from sayings import goodbye
 
 if len(sys.argv) == 2:
-    hello(sys.argv[1])
+    goodbye(sys.argv[1])
 
 # This gets printed out when we run "python3 say.py Carlos":
 # Hello world!
 # Goodbye world!
 # Hello Carlos!
+
+# now when the program is ran with the same command to the terminal, this prints out:
+# Hello Carlos!
+# Why? Because it is going to ignore the call to main() because it is wrapped in that conditional statement.
+# To use the other function created in sayings.py, we need to import that function. from sayings import goodbye
--- sidequests/own_libraries/sayings.py
+++ sidequests/own_libraries/sayings.py
@@ -11,5 +11,7 @@ def goodbye(name):
     print(f"Goodbye {name}!")
 
 
-main()  # it seems that what we want to print int the say.py file is not properly printing because we are blindly
+if __name__ == "__main__":
+    main()  # it seems that what we want to print int the say.py file is not properly printing because we are blindly
 # calling the function main(). Main() gets called no matter what.
+# __name__ is a variable that is set to the name "main" of the current module when ran in the command line.
