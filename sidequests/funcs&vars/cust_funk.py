4ec71b40bdf5ec4e4e7419954698b263119e2721
Closdlgdo <cidstp031921@gmail.com>
2023-10-28 20:54:46 -0700

    We added and called some parameters in our function, thanks to Programming with Mosh - Python Functions.

--- sidequests/funcs&vars/cust_funk.py
+++ sidequests/funcs&vars/cust_funk.py
@@ -1,9 +1,9 @@
 # make sure your functions are meaningful and descriptive. Use lowercase letters for your function names and underscores
 # for multiple words in your function name.
 
-def family():  # our function doesnt take an input.
-    print("Hello world!")  # the print function takes an input.
-    print("Goodbye world!")
+def family(first_name, last_name):  # let's add some parameters to the function.
+    print(f"Hello {first_name} {last_name}!")  # the print function takes an input.
+    print(f"Goodbye {first_name} {last_name}!")
 
 
-family()  # calling the function.
+family("Carlos", "Delgado")  # calling the function with arguments.
