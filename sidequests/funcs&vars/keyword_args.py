c2269db8ba179425e0301a446285fd23ccd09b3d
Closdlgdo <cidstp031921@gmail.com>
2023-10-29 11:42:15 -0700

    increment a number by a given value by removing the variable and add the increment function to the print function, thanks to Programming with Mosh - Python Functions.

--- sidequests/funcs&vars/keyword_args.py
+++ sidequests/funcs&vars/keyword_args.py
@@ -3,5 +3,8 @@ def increment(number, by):
     return number + by
 
 
-result = increment(5, 10)
-print(result)  # this is to print out in the terminal.
+# result = increment(5, 10)  # we can simplify this code by removing the variable.
+print(increment(5, 10))  # this is to print out in the terminal.
+# we replace result with the call to increment function.
+# it first calls the increment function and then returns the result and temporarily store the result in the variable we
+# cannot see. then it passes that variable as an argument to the print function.
