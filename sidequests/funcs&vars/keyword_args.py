aceb108ae5f01a9fa15fcb9e4fa8631728dbd5b4
Closdlgdo <cidstp031921@gmail.com>
2023-10-29 11:44:30 -0700

    increment a number by a given value using keyword arguments in our print function, thanks to Programming with Mosh - Python Functions.

--- sidequests/funcs&vars/keyword_args.py
+++ sidequests/funcs&vars/keyword_args.py
@@ -3,8 +3,13 @@ def increment(number, by):
     return number + by
 
 
-# result = increment(5, 10)  # we can simplify this code by removing the variable.
-print(increment(5, 10))  # this is to print out in the terminal.
-# we replace result with the call to increment function.
-# it first calls the increment function and then returns the result and temporarily store the result in the variable we
-# cannot see. then it passes that variable as an argument to the print function.
+# # result = increment(5, 10)  # we can simplify this code by removing the variable.
+# print(increment(5, 10))  # this is to print out in the terminal.
+# # we replace result with the call to increment function.
+# # it first calls the increment function and then returns the result and temporarily store the result in the variable we
+# # cannot see. then it passes that variable as an argument to the print function.
+
+################################
+
+# to make the code simpler, we can use *keyword arguments* in our print function.
+print(increment(number=5, by=10))
