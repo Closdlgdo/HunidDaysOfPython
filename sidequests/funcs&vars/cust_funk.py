3005236310a9379ff045fff9b8058e4a96bf6afe
Closdlgdo <cidstp031921@gmail.com>
2023-10-28 20:51:04 -0700

    Practicing creating functions, thanks to Programming with Mosh - Python Functions.

---
+++ sidequests/funcs&vars/cust_funk.py
@@ -0,0 +1,9 @@
+# make sure your functions are meaningful and descriptive. Use lowercase letters for your function names and underscores
+# for multiple words in your function name.
+
+def family():  # our function doesnt take an input.
+    print("Hello world!")  # the print function takes an input.
+    print("Goodbye world!")
+
+
+family()  # calling the function.
