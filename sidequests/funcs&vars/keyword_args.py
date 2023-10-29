32ff814c349081c7c41ffa82a5188dfe6708ba9c
Closdlgdo <cidstp031921@gmail.com>
2023-10-29 11:38:38 -0700

    increment a number by a given value, thanks to Programming with Mosh - Python Functions.

---
+++ sidequests/funcs&vars/keyword_args.py
@@ -0,0 +1,7 @@
+# we want to increment the number by a given value.
+def increment(number, by):
+    return number + by
+
+
+result = increment(5, 10)
+print(result)  # this is to print out in the terminal.
