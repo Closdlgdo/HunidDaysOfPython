4d07643825ccbca9c00a70c1180b32befe88619c
Closdlgdo <cidstp031921@gmail.com>
2023-10-29 21:57:56 -0700

    we will be creating a function that take variable number of arguments and prints out a tuple, thanks to Programming with Mosh - Python Functions.

--- sidequests/funcs&vars/var_args.py
+++ sidequests/funcs&vars/var_args.py
@@ -1,7 +1,8 @@
 # we will create a new function that takes a variable number of arguments.
-def multiply(x, y):  # our function only takes two parameters.
-    return x * y
+def multiply(*numbers):  # we replace the two numbers with a single parameter using a plural name and a *.
+    # return x * y
+    print(numbers)  # this will print the tuple of numbers.
 
 
-print(multiply(2, 3))
+multiply(7, 3, 2, 6)
 # print(multiply(2, 3, 4, 5))  # this won't work because we have only 2 arguments in the function.
