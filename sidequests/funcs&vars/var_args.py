d5087aa0aabdc61aa3d02f956c0786376ae3857d
Closdlgdo <cidstp031921@gmail.com>
2023-10-30 04:00:52 -0700

    we are iterating within the tuple so we can print out every number, thanks to Programming with Mosh - Python Functions.

--- sidequests/funcs&vars/var_args.py
+++ sidequests/funcs&vars/var_args.py
@@ -2,6 +2,10 @@
 def multiply(*numbers):  # we replace the two numbers with a single parameter using a plural name and a *.
     # return x * y
     print(numbers)  # this will print the tuple of numbers.
+    # a tuple is similar to a list, but it is immutable which means that it cannot be modified.
+    # they are like a list in the sense that iterate over them using loops.
+    for number in numbers:
+        print(number)
 
 
 multiply(7, 3, 2, 6)
