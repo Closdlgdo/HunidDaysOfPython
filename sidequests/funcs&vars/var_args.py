6f35727f066b3c7d6b5c4c56333d8c70a1a92bf8
Closdlgdo <cidstp031921@gmail.com>
2023-10-29 21:53:02 -0700

    we will be creating a function that take variable number of arguments, thanks to Programming with Mosh - Python Functions.

---
+++ sidequests/funcs&vars/var_args.py
@@ -0,0 +1,7 @@
+# we will create a new function that takes a variable number of arguments.
+def multiply(x, y):  # our function only takes two parameters.
+    return x * y
+
+
+print(multiply(2, 3))
+# print(multiply(2, 3, 4, 5))  # this won't work because we have only 2 arguments in the function.
