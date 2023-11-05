957f98db1422a62b3df0292bb453b83228207b60
Closdlgdo <cidstp031921@gmail.com>
2023-11-05 05:39:22 -0700

    CS50 Unit testing continued. We are using pytest to help with Unit testing in the hello.py file.

--- sidequests/unit_tests/test_hello.py
+++ sidequests/unit_tests/test_hello.py
@@ -1,7 +1,10 @@
 from hello import hello
 
 
-def test_hello():
+def test_default():
     assert hello("Carlos") == "Hello Carlos!"  # This will not work because it is not returning a value.
+
+
+def test_argument():
     assert hello() == "Hello world!"  # We call hello without any parameters. It is not great practice to have multiple
     # tests in the same function.
