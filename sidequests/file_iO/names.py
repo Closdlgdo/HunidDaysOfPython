709520a244b844017a0c7bd37f6a6a1ad97cc22e
Closdlgdo <cidstp031921@gmail.com>
2023-11-09 11:17:16 -0700

    We were able to change the code where the "w" function was changed to "a" which means append, CS50 with David Malan.

--- sidequests/file_iO/names.py
+++ sidequests/file_iO/names.py
@@ -9,14 +9,21 @@
 # # Every time we rerun the program, the names do not get saved. They will simply disappear and will
 # # be replaced with the new names. If a program required the names to be saved, then this would not work out.
 
+# names = input("Please enter your name: ")
+#
+# file = open("names.txt", "w")  # we write two arguments in the function. One is the name of the file we want to use,
+# # and the other is what we want to do to the file. "w" means write into the file. If the file doesn't exist, it will create
+# # a new one.
+# file.write(names)
+# file.close()
+# # Instead of printing out the name that has just been inputted, we will save it to a file by using the open() function.
+# # Allows us to read from or write to a file.
+# # This certain code will not save all the names when the program is re-run over and over since there is no list to save in,
+# # and we are not appending to the file. The "w" writes over the previous contents of the file if not properly appended.
+
 names = input("Please enter your name: ")
 
-file = open("names.txt", "w")  # we write two arguments in the function. One is the name of the file we want to use,
-# and the other is what we want to do to the file. "w" means write into the file. If the file doesn't exist, it will create
-# a new one.
-file.write(names)
+file = open("names.txt", "a")  # When we use the "a" function, we are appending (adding to the list) to the file instead
+# of writing over it.
+file.write(f"{names}\n")
 file.close()
-# Instead of printing out the name that has just been inputted, we will save it to a file by using the open() function.
-# Allows us to read from or write to a file.
-# This certain code will not save all the names when the program is re-run over and over since there is no list to save in,
-# and we are not appending to the file. The "w" writes over the previous contents of the file if not properly appended.
