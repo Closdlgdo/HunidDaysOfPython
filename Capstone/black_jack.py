2c10101320abee241505fe6114e75591cd024e57
Closdlgdo <cidstp031921@gmail.com>
2023-11-09 03:58:10 -0700

    Back to 100 DOP, We are now working into our first Capstone Project. Creating a black jack game. We have created a few functions that are key components for the game.

--- Capstone/black_jack.py
+++ Capstone/black_jack.py
@@ -1,12 +1,46 @@
 # This is our first Capstone project for 100 Days of Python.
 # For this Capstone project, we will be creating a Black Jack game.
-
+import random
 # House Rules:
 # 1. The deck is unlimited in size.
 # 2. There are no jokers.
 # 3. The Jack/Queen/King all count as 10.
 # 4. The Ace can count as 11 or 1.
 # 5. use the following list as the deck of cards:
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # 6. The cards in the list have equal probability of being drawn.
 # 7. Cards are not removed from the deck as they are drawn.
+
+
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
