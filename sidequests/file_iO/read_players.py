74f2822e118132b930ccd19435c9eab6b1a4b02d
Closdlgdo <cidstp031921@gmail.com>
2023-11-11 04:57:17 -0700

    We reformatted the code to create a dictionary within the list of players/teams, Un-Sorted. CS50 with David Malan.

--- sidequests/file_iO/read_players.py
+++ sidequests/file_iO/read_players.py
@@ -2,8 +2,20 @@ player_s = []
 
 with open("players.csv") as file:
     for line in file:
-        players, team = line.rstrip().split(",")
-        player_s.append(f"{players} plays/ed with {team}")
+        players, team = line.rstrip().split(",")  # This line splits each line of the file into two parts using the
+        # split(",") method.
+        player = {"name": players, "team": team}  # This creates a dictionary named player with keys "name" and "team"
+        # and assigns the values from the previous line.
+        player_s.append(player)
 
-for player in sorted(player_s):
-    print(player)
+
+def get_player(player):
+    return player["name"]
+
+
+# ^^^^^^^^ This defines a function get_player that takes a player dictionary as an argument and returns the value associated
+# with the key "name". This function will be used as the key function for sorting the players.
+
+for player in sorted(player_s, key=get_player):  # This iterates through the sorted player_s list based on the "name"
+    # key using the get_player function.
+    print(f"{player['name']} plays for the {player['team']}.")
