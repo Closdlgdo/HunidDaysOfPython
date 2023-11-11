7872ee605069ced6ee9c1b1293b368b193e9d43e
Closdlgdo <cidstp031921@gmail.com>
2023-11-10 22:24:15 -0700

    Instead of printing each name as it was inputted, we are sorting it first then printing out the sorted version, CS50 with David Malan.

--- sidequests/file_iO/read_names.py
+++ sidequests/file_iO/read_names.py
@@ -1,8 +1,19 @@
-# This particular file with read the names from names.txt.
-with open("names.txt", 'r') as file:
+# # This particular file with read the names from names.txt.
+# with open("names.txt", 'r') as file:
+#     for line in file:
+#         print(line.rstrip())  # this shortens the code below and makes it cleaner.
+# #     lines = file.readlines()  # readlines is a special method that returns all the lines from the file as a list.
+# #
+# # for line in lines:  # we want to iterate over the list of lines to print out each name.
+# #     print(line.rstrip())
+
+################################################################
+# We want to print the names from names.txt in a sorted order. To do this, we can use the sorted() function.
+names = []  # we create an empty list
+
+with open("names.txt") as file:  # we do not need to specify 'r' for read.
     for line in file:
-        print(line.rstrip())  # this shortens the code below and makes it cleaner.
-#     lines = file.readlines()  # readlines is a special method that returns all the lines from the file as a list.
-#
-# for line in lines:  # we want to iterate over the list of lines to print out each name.
-#     print(line.rstrip())
+        names.append(line.rstrip())  # we append the names to the list
+
+for name in sorted(names):
+    print(f"Hello, {name}!")
