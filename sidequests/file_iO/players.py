034a54fc39656e9471d77bbb4b25decc78879f0d
Closdlgdo <cidstp031921@gmail.com>
2023-11-12 07:21:20 -0700

    Just added more players to my csv file. CS50 with David Malan.

--- sidequests/file_iO/players.py
+++ sidequests/file_iO/players.py
@@ -1,4 +1,4 @@
-names = input("Please enter your favorite player's name and team seperated by a comma: ")
+names = input("Enter your favorite player's name, team and number seperated by a comma: ")
 
 with open("players.csv",
           "a") as file:  # When we use the "a" function, we are appending (adding to the list) to the file instead
