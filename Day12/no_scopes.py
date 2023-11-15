9a5e0f1622f67fedc6e0333c27fb01f4b254c0bb
Closdlgdo <cidstp031921@gmail.com>
2023-11-15 04:59:20 -0700

    Learning about scopes, 100DOP with Dr. Angela Yu

--- Day12/no_scopes.py
+++ Day12/no_scopes.py
@@ -18,3 +18,14 @@ def global_scope():
         print(x * 2)  # this print function is in the local scope because it is indented within the function.
 
 # Scopes are also a concept with functions, not only variables.
+# There is no such concept of Block scope in Python.
+game_level = 3
+
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]  # if you create a variable within the function then it is only available
+    # within that function
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
