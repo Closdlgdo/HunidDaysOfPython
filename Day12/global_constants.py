bff56a9924c0458945fbe0d79cb693c385e08728
Closdlgdo <cidstp031921@gmail.com>
2023-11-15 05:19:09 -0700

    Learning about global scopes, 100DOP with Dr. Angela Yu

---
+++ Day12/global_constants.py
@@ -0,0 +1,9 @@
+# Global Constants using the global keyword.
+
+PI = 3.14159  # you want to look it up once and never have to look it up ever again.
+
+
+# And if you want to use it within a function all you have to call is the name.
+
+def calc():
+    print(PI)
