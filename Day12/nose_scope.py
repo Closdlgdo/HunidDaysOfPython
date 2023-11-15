a02e38cba201c2a1610f3c126e9865436cd903b0
Closdlgdo <cidstp031921@gmail.com>
2023-11-15 05:14:27 -0700

    Learning about changing global scopes, 100DOP with Dr. Angela Yu

--- Day12/nose_scope.py
+++ Day12/nose_scope.py
@@ -4,14 +4,16 @@ enemies = 1
 
 
 def increase_enemies():  # this function creates a local scope
-    global enemies  # this allows us to bring in the global scope into the function in order to be changed.
-    # You wouldn't want to do this often because it is prone to bugs and errors.
-    enemies += 3
+    # global enemies  # this allows us to bring in the global scope into the function in order to be changed.
+    # # You wouldn't want to do this often because it is prone to bugs and errors.
+    # enemies += 3
     print(f"enemies inside function: {enemies}")
     # This prints out whatever is within the scope, so the output would be 3.
     # It is bad practice to name your local and global scopes the same.
+    return enemies + 1
 
 
-increase_enemies()
+enemies = increase_enemies()  # once this function is called, you'll get hold of the outputs and you can save it to the global
+# variable; enemies.
 print(f"enemies outside function: {enemies}")
 # Ths prints out whatever is outside the scope, so the output would be 1.
