2c071a2eb4d54acdb6453e6a79c266d429c63c80
Closdlgdo <cidstp031921@gmail.com>
2023-11-17 05:32:59 -0700

    The bug in this program is that if I input 1994 which is the cutoff, it doesn't print out anything, 100DOP with Dr. Angela Yu

--- Day13/play_computer.py
+++ Day13/play_computer.py
@@ -1,7 +1,7 @@
 # Play Computer
 
 year = int(input("What year were you born? "))
-if year > 1980 and year < 1994:
-    print("You are a millenial."
-    elif year > 1994:
+if 1980 < year < 1994:
+    print("You are a millenial.")
+elif year > 1994:
     print("You are a Gen Z.")
