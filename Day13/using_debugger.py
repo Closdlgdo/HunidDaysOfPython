6d0705ed2745f4902607843c0d829cfb1e591601
Closdlgdo <cidstp031921@gmail.com>
2023-11-19 05:12:23 -0700

    Working with fixing bugs within my code, 100DOP with Dr. Angela Yu

---
+++ Day13/using_debugger.py
@@ -0,0 +1,11 @@
+# We will use the debugger.
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
