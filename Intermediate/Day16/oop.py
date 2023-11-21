c29bf58db47c177848d9ebd3d78b8e6e36c81fc7
Closdlgdo <cidstp031921@gmail.com>
2023-11-21 04:55:51 -0700

    Starting to learn OOP, from 100DOP with Dr. Angela Yu

---
+++ Intermediate/Day16/oop.py
@@ -0,0 +1,8 @@
+# What is Object Oriented Programming?
+# Object Oriented Programming (OOP) is a programming paradigm that uses objects and classes in programming.
+# The idea is to create real-world entities that have attributes (data) and behaviors (functions).
+# An object is an instance of a class. A class can have many objects. A class can have many methods.
+# A class can have many attributes.
+# A method is a function that is defined inside a class.
+# A object is an instance of a class.
+# An attribute is a property of an object.
