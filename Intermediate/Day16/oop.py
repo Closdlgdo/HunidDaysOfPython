a35dfb9effbd8f12cc50587888845419de5c13de
Closdlgdo <cidstp031921@gmail.com>
2023-11-21 05:26:39 -0700

    Starting to learn OOP by importing a module called Turtle, from 100DOP with Dr. Angela Yu

--- Intermediate/Day16/oop.py
+++ Intermediate/Day16/oop.py
@@ -2,7 +2,18 @@
 # Object Oriented Programming (OOP) is a programming paradigm that uses objects and classes in programming.
 # The idea is to create real-world entities that have attributes (data) and behaviors (functions).
 # An object is an instance of a class. A class can have many objects. A class can have many methods.
-# A class can have many attributes.
+# A class can have many attributes. A class is basically a blueprint for an object.
 # A method is a function that is defined inside a class.
-# A object is an instance of a class.
+# An object is an instance of a class.
 # An attribute is a property of an object.
+from turtle import Turtle, Screen
+
+pedro = Turtle()
+print(pedro)
+pedro.shape("turtle")
+pedro.color("DarkSeaGreen4")
+pedro.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
