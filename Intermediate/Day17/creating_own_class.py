46eabbd0e4bf21d1d1cbf03fad43bf69569ff2b8
Closdlgdo <cidstp031921@gmail.com>
2023-11-24 06:48:35 -0700

    made an init function that gets called from the objects created, from 100DOP with Dr. Angela Yu

--- Intermediate/Day17/creating_own_class.py
+++ Intermediate/Day17/creating_own_class.py
@@ -3,7 +3,10 @@
 # Pascal case is: CarlosDelgado, HelloWorld, MyNewClass.
 
 class User:
-    pass
+    def __init__(self):
+        # This is where we initialize or create the starting values of the attributes.
+        # The init function wil be called when we create a new object from this class.
+        print("New User Created!")
 
 
 user_1 = User()  # user_1 is an object of the User class
@@ -11,6 +14,12 @@ user_1.id = "001"  # this is an attribute of the user_1 object
 user_1.username = "Carlos"  # this is an attribute of the user_1 object
 
 print(user_1.username)
+
+user_2 = User()
+user_2.id = "002"
+user_2.username = "Theano"
+print(user_2.username)
+
 
 # An attribute is a variable that can be associated with an object almost as if
 # we created a new variable, except we attach it to an object.
