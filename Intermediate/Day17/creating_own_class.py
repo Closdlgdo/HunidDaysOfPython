2f185ca3ba9e5229132146623dd261155840b814
Closdlgdo <cidstp031921@gmail.com>
2023-11-24 06:57:00 -0700

    We will begin to create our quiz game, from 100DOP with Dr. Angela Yu

--- Intermediate/Day17/creating_own_class.py
+++ Intermediate/Day17/creating_own_class.py
@@ -3,22 +3,34 @@
 # Pascal case is: CarlosDelgado, HelloWorld, MyNewClass.
 
 class User:
-    def __init__(self):
+    def __init__(self, user_id, username):
         # This is where we initialize or create the starting values of the attributes.
         # The init function wil be called when we create a new object from this class.
-        print("New User Created!")
+        self.id = user_id
+        self.username = username
+        self.followers = 0  # These two are called default attributes.
+        self.following = 0
+
+    def follow(self, user):  # When a user decides to follow another user.
+        user.followers += 1  # Their follower account goes up by one.
+        self.following += 1  # Our following account goes up by one.
 
 
-user_1 = User()  # user_1 is an object of the User class
-user_1.id = "001"  # this is an attribute of the user_1 object
-user_1.username = "Carlos"  # this is an attribute of the user_1 object
+user_1 = User("001", "Carlos")  # user_1 is an object of the User class
+# user_1.id = "001"  # this is an attribute of the user_1 object
+# user_1.username = "Carlos"  # this is an attribute of the user_1 object
+user_2 = User("002", "Theano")
 
-print(user_1.username)
+user_1.follow(user_2)  # user_1(object) will follow(method) user_2
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
 
-user_2 = User()
-user_2.id = "002"
-user_2.username = "Theano"
-print(user_2.username)
+# user_2 = User()
+# user_2.id = "002"
+# user_2.username = "Theano"
+# print(user_2.username)
 
 
 # An attribute is a variable that can be associated with an object almost as if
@@ -29,3 +41,6 @@ print(user_2.username)
 
 # The way to create the constructor is:
 # def __init__(self): this is used to initialize the attributes.
+# The way to create an attribute is:
+# def __init__(self, parameter):
+#     self.parameter = parameter
