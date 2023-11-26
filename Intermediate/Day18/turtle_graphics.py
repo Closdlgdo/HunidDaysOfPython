85462a70cdac084403a3fc3e1ac3f91f5b14b4b3
Closdlgdo <cidstp031921@gmail.com>
2023-11-26 07:20:22 -0700

    Pancho creates multiple shapes on each other, from 100DOP with Dr. Angela Yu

--- Intermediate/Day18/turtle_graphics.py
+++ Intermediate/Day18/turtle_graphics.py
@@ -4,24 +4,37 @@ pancho = Turtle()
 pancho.shape("turtle")
 pancho.color("DarkSeaGreen4")
 
+for _ in range(3):
+    pancho.forward(100)
+    pancho.right(120)
+
 for _ in range(4):
-    pancho.forward(10)
-    pancho.penup()
-    pancho.forward(10)
-    pancho.pendown()
-    pancho.forward(10)
-    pancho.penup()
-    pancho.forward(10)
-    pancho.pendown()
-    pancho.forward(10)
-    pancho.penup()
-    pancho.forward(10)
-    pancho.pendown()
-    pancho.forward(10)
-    pancho.penup()
-    pancho.forward(10)
-    pancho.pendown()
+    pancho.forward(100)
     pancho.right(90)
+
+for _ in range(5):
+    pancho.forward(100)
+    pancho.right(72)
+
+for _ in range(6):
+    pancho.forward(100)
+    pancho.right(60)
+
+for _ in range(7):
+    pancho.forward(100)
+    pancho.right(51.5)
+
+for _ in range(8):
+    pancho.forward(100)
+    pancho.right(45)
+
+for _ in range(9):
+    pancho.forward(100)
+    pancho.right(40)
+
+for _ in range(10):
+    pancho.forward(100)
+    pancho.right(36)
 
 screen = Screen()
 screen.exitonclick()
