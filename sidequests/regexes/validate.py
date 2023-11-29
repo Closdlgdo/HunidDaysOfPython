5e997af9b7cfa8762c45fef6ba18bbdbcb70b875
Closdlgdo <cidstp031921@gmail.com>
2023-11-29 11:48:11 -0700

    Practicing with regex as well as email validation. CS50 w/ David Malan

--- sidequests/regexes/validate.py
+++ sidequests/regexes/validate.py
@@ -2,11 +2,17 @@
 
 email = input("Email: ").strip()
 
-if "@" in email and "." in email:
-    print("Valid")
+username, domain = email.split("@")
+
+if username and domain.endswith(".edu"):
+    print("Valid email!")
 else:
-    print("Invalid")
+    print("Invalid email!")
+
 
 # This program is buggy because we set a very low bar. we only look for the @ so we can only input the @ and nothing else
 # and the code would still call it as valid.
 # This doesn't fix the code because we only look for the @ and a dot and nothing else.
+
+# Now we are creating the code more valid but it is still buggy. Mainly because we can give a sketchy
+# email address like "hello@.edu" and the code will still call it valid.
