6a9bfd12ff2f9e7509387491e01837dc8b8d2cd5
Closdlgdo <cidstp031921@gmail.com>
2023-11-29 11:54:55 -0700

    Practicing with regex as well as email validation. CS50 w/ David Malan

--- sidequests/regexes/validate.py
+++ sidequests/regexes/validate.py
@@ -1,14 +1,14 @@
-# Regex is a regular expression that matches a string against a pattern.
-
-email = input("Email: ").strip()
-
-username, domain = email.split("@")
-
-if username and domain.endswith(".edu"):
-    print("Valid email!")
-else:
-    print("Invalid email!")
-
+# # Regex is a regular expression that matches a string against a pattern.
+#
+# email = input("Email: ").strip()
+#
+# username, domain = email.split("@")
+#
+# if username and domain.endswith(".edu"):
+#     print("Valid email!")
+# else:
+#     print("Invalid email!")
+#
 
 # This program is buggy because we set a very low bar. we only look for the @ so we can only input the @ and nothing else
 # and the code would still call it as valid.
@@ -16,3 +16,22 @@ else:
 
 # Now we are creating the code more valid but it is still buggy. Mainly because we can give a sketchy
 # email address like "hello@.edu" and the code will still call it valid.
+
+#####################################################################################
+# In python there is a library called re that can help us with regular expressions.
+# re.search(pattern, string, flags=0)
+# Pattern: is what we want to search for in the string that came from the user.
+# string: is the string that we want to search.
+# flags: is a flag that we can use to control the search.
+
+import re
+
+email = input("Email: ").strip()
+
+if re.search("@", email):
+    print("Valid email!")
+else:
+    print("Invalid email!")
+
+# This program is buggy because we set a very low bar. we only look for the @ so we can only input the @ and nothing else
+# and the code would still call it as valid.
