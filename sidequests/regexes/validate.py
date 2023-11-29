d6782eaa1df928c3919a42d7f6ac8a577edf1616
Closdlgdo <cidstp031921@gmail.com>
2023-11-29 11:42:04 -0700

    Practicing with regex as well as email validation. CS50 w/ David Malan

--- sidequests/regexes/validate.py
+++ sidequests/regexes/validate.py
@@ -2,10 +2,11 @@
 
 email = input("Email: ").strip()
 
-if "@" in email:
+if "@" in email and "." in email:
     print("Valid")
 else:
     print("Invalid")
 
 # This program is buggy because we set a very low bar. we only look for the @ so we can only input the @ and nothing else
 # and the code would still call it as valid.
+# This doesn't fix the code because we only look for the @ and a dot and nothing else.
