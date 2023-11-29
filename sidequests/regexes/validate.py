14474749055169897286fec48761d3f157a1b459
Closdlgdo <cidstp031921@gmail.com>
2023-11-29 11:38:44 -0700

    Practicing with regex as well as email validation. CS50 w/ David Malan

---
+++ sidequests/regexes/validate.py
@@ -0,0 +1,11 @@
+# Regex is a regular expression that matches a string against a pattern.
+
+email = input("Email: ").strip()
+
+if "@" in email:
+    print("Valid")
+else:
+    print("Invalid")
+
+# This program is buggy because we set a very low bar. we only look for the @ so we can only input the @ and nothing else
+# and the code would still call it as valid.
