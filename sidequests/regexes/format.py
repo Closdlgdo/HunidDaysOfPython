9b67c9f3765cdd33b778dd3959a878efd7e439d8
Closdlgdo <cidstp031921@gmail.com>
2023-11-29 21:22:35 -0700

    Practicing with regex as well as email formatting. Check the notes left, CS50 w/ David Malan

--- sidequests/regexes/format.py
+++ sidequests/regexes/format.py
@@ -17,22 +17,37 @@
 
 ################################################################################################
 ################################################################################################
+# import re
+#
+# name = input("Name: ").strip()
+# matches = re.search(r"^(.+), (.+)$", name)  # we deliberately capture the dot plus for capturing purposes
+#
+# if matches:  # we can be more explicit by getting specific groups back by doing this:
+#     # last = matches.group(1)  # before comma ^^^^^
+#     # first = matches.group(2)  # after comma ^^^^^
+#     # We can refine this code a bit further by:
+#     name = matches.group(2) + " " + matches.group(1)
+#
+# print(f"Hello, {name}!")
+#
+# # When we used re.search(), we used it to answer a question: "Does the users input mathe the following pattern?"
+# # you can also assign the re.search() to a variable to get a more precise answer to what has been found when searched for.
+# # the "A|B" is a special character that means match either A or B.
+# # the "(...)" is a special character that means match the expression inside the parentheses.
+# # the "(?:...)" is a special character that means match the expression inside the parentheses but do not capture the match.
+# # This code however does have some bugs that need fixing. For example: "Name: last,first" will be "Hello, last,first"
+
+################################################################################################
+################################################################################################
+
 import re
 
 name = input("Name: ").strip()
-matches = re.search(r"^(.+), (.+)$", name)  # we deliberately capture the dot plus for capturing purposes
+matches = re.search(r"^(.+), ?(.+)$", name)  # the addition of the "?" will tolerate more whitespaces.
 
-if matches:  # we can be more explicit by getting specific groups back by doing this:
-    # last = matches.group(1)  # before comma ^^^^^
-    # first = matches.group(2)  # after comma ^^^^^
-    # We can refine this code a bit further by:
+if matches:
     name = matches.group(2) + " " + matches.group(1)
 
 print(f"Hello, {name}!")
 
-# When we used re.search(), we used it to answer a question: "Does the users input mathe the following pattern?"
-# you can also assign the re.search() to a variable to get a more precise answer to what has been found when searched for.
-# the "A|B" is a special character that means match either A or B.
-# the "(...)" is a special character that means match the expression inside the parentheses.
-# the "(?:...)" is a special character that means match the expression inside the parentheses but do not capture the match.
-# This code however does have some bugs that need fixing. For example: "Name: last,first" will be "Hello, last,first"
+# the "?" is a special character that means match zero or one of the preceding characters.
