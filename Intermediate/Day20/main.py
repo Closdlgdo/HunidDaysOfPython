6f502031a97d06fd384a614ab3e13d58c955fadf
Closdlgdo <cidstp031921@gmail.com>
2023-12-01 05:30:31 -0700

    Creating a snake game, with Angela Yu in 100DOP

---
+++ Intermediate/Day20/main.py
@@ -0,0 +1,54 @@
+# We will be creating a game of Snake.
+# The seven steps that it takes to create the game:
+# 1. Create the snake body
+# 2. Move the snake
+# 3. Create the food
+# 4. Detect collision with food
+# 5. Create the scoreboard
+# 6. Detect collision with wall
+# 7. Detect collision with tail
+import time
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+#
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+#
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+# We can also do this with a for loop and a tuple.
+# Create the snake body. Each turtle is 20x20 pixels.
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+# Move the snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
+screen.exitonclick()
