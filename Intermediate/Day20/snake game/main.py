c8c45304070ffbe12a23342890be79d0d7419c33
Closdlgdo <cidstp031921@gmail.com>
2023-12-03 06:38:30 -0700

    Creating a snake game, with Angela Yu in 100DOP

--- Intermediate/Day20/snake game/main.py
+++ Intermediate/Day20/snake game/main.py
@@ -17,7 +17,7 @@ from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("black")
+screen.bgcolor("purple")
 screen.title("Snake Game")
 screen.tracer(0)
 
--- Intermediate/Day20/snake game/snake.py
+++ Intermediate/Day20/snake game/snake.py
@@ -20,7 +20,7 @@ class Snake:
 
     def add_segment(self, position):
         new_segment = Turtle("square")
-        new_segment.color("white")
+        new_segment.color("gold")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
@@ -53,7 +53,7 @@ class Snake:
 
     def add_segment(self, position):
         new_segment = Turtle("square")
-        new_segment.color("white")
+        new_segment.color("gold")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
