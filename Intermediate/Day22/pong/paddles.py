773d2fff31104de11d1c442eaae704f43ac54fdb
Closdlgdo <cidstp031921@gmail.com>
2023-12-04 05:45:08 -0700

    Creating the Pong game, with Angela Yu in 100DOP

---
+++ Intermediate/Day22/pong/paddles.py
@@ -0,0 +1,21 @@
+# This is the paddle class
+class Paddle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.paddle = Turtle()
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+        self.paddle.penup()
+        self.paddle.goto(x, y)
+
+    def move_up(self):
+        y = self.paddle.ycor()
+        y += 20
+        self.paddle.sety(y)
+
+    def move_down(self):
+        y = self.paddle.ycor()
+        y -= 20
+        self.paddle.sety(y)
