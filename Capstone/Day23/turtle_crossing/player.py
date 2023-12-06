713947597436fb947f3973cb3ae18c1b6b2f7bd8
Closdlgdo <cidstp031921@gmail.com>
2023-12-06 04:45:43 -0700

    Creating the Turtle Crossing game, with Angela Yu in 100DOP

--- Capstone/Day23/turtle_crossing/player.py
+++ Capstone/Day23/turtle_crossing/player.py
@@ -1,7 +1,28 @@
+import turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
-    pass
+    def __init__(self):
+        self.player = None
+        self.create_player()
+
+    def create_player(self):
+        self.player = turtle.Turtle()
+        self.player.shape("turtle")
+        self.player.color("black")
+        self.player.penup()
+        self.player.goto(STARTING_POSITION)
+        self.player.setheading(90)
+
+    def go_up(self):
+        self.player.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.player.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
