a47f4653c8206a805ada7f66be344ce01dca9762
Closdlgdo <cidstp031921@gmail.com>
2023-12-07 04:37:34 -0700

    Creating a score section on our snake game, with Angela Yu in 100DOP

--- Intermediate/Day20/snake game/main.py
+++ Intermediate/Day20/snake game/main.py
@@ -48,8 +48,10 @@ while game_is_on:
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset()
+            snake.reset()
