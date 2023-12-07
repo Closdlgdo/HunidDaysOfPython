67c85a60c47ec2744df845e79a74c8bf7b2501bd
Closdlgdo <cidstp031921@gmail.com>
2023-12-07 04:45:53 -0700

    we are learning about file i/o, with Angela Yu in 100DOP

--- Intermediate/Day24/main.py
+++ Intermediate/Day24/main.py
@@ -1,3 +1,22 @@
-file = open("my_file.txt")
-content = file.read()
-print(content)
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew text.")
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")
+
+with open("new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("new_file.txt", mode="a") as file:
+    content = file.read()
+    print(content)
+# file.close()  # always remember to close the file after using it because it continues to run in the background.
