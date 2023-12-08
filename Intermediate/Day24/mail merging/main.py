e8e067c1b7524d6e14829a41fc805eb7ee067212
Closdlgdo <cidstp031921@gmail.com>
2023-12-08 05:09:37 -0700

    We are trying to create a automatic name insert in a letter, with Angela Yu in 100DOP

--- Intermediate/Day24/mail merging/main.py
+++ Intermediate/Day24/mail merging/main.py
@@ -1,14 +1,18 @@
 # Create a letter using first_letter.txt and replace the [name] with the name of the guest from the invited_people.txt
 # Save the letters in the folder ready_to_send.
 
-with open("../names/invited_people.txt") as file:
+# Read names from 'invited_people.txt'
+with open("names/invited_people") as file:
     names = file.readlines()
 
-with open("../letters/first_letter.txt") as file:
+# Read the template letter
+with open("letters/first_letter") as file:
     letter = file.read()
 
+# Create personalized letters
 for name in names:
     name = name.strip()
     new_letter = letter.replace("[name]", name)
-    with open(f"ready_to_send/letter_for_{name}.txt", mode="w") as file:
+    output_path = f"./output/ready_to_send/letter_for_{name}.txt"
+    with open(output_path, mode="w") as file:
         file.write(new_letter)
