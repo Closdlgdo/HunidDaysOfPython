3ef92efd858111e94c97f69c08fefea29031ffb7
Closdlgdo <cidstp031921@gmail.com>
2023-12-09 04:55:22 -0700

    We are learning about csv, with Angela Yu in 100DOP

--- Intermediate/Day25/csv/main.py
+++ Intermediate/Day25/csv/main.py
@@ -13,4 +13,9 @@
 import pandas as pd
 
 data = pd.read_csv("weather_data.csv")
-print(data)
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+# let's find the average temperature
+print(sum(temp_list) / len(temp_list))
