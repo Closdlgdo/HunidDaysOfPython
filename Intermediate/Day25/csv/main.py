8c529198ed3ac2fad1334896dee681f93abaaf0b
Closdlgdo <cidstp031921@gmail.com>
2023-12-09 05:10:00 -0700

    We are learning about csv, with Angela Yu in 100DOP

--- Intermediate/Day25/csv/main.py
+++ Intermediate/Day25/csv/main.py
@@ -14,7 +14,6 @@ import pandas as pd
 
 data = pd.read_csv("weather_data.csv")
 data_dict = data.to_dict()
-print(data_dict)
 
 temp_list = data["temp"].to_list()
 # # let's find the average temperature
@@ -26,13 +25,18 @@ temp_list = data["temp"].to_list()
 # # This will find the max temperature
 # print(data["temp"].max())
 
-# Get data in columns
-print(data["condition"])
-print(data.condition)
-
-# Get data in row
-print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max()])
+# # Get data in columns
+# print(data["condition"])
+# print(data.condition)
+#
+# # Get data in row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
 
 monday = data[data.day == "Monday"]
-print(monday.condition)
+# print(monday.condition)
+
+# Let's find the temp for monday and convert it to fahrenheit
+monday_temp = int(monday.temp)
+monday_temp_fahrenheit = monday_temp * 1.8 + 32
+print(monday_temp_fahrenheit)
