47ec21a0ac28d521a4e6cf91f6797e5f26fd87ec
Closdlgdo <cidstp031921@gmail.com>
2023-12-12 04:31:22 -0700

    We are learning about list comprehension, with Angela Yu in 100DOP

--- Intermediate/Day26/nato.py
+++ Intermediate/Day26/nato.py
@@ -1,12 +1,24 @@
 # Today we are learning about list comprehensions. A list comprehension is a way to create a new list with less syntax.
 # Example:
 # a_list = [1, 2, 3, 4, 5]
-# new_list = [new_item for item in list]
+# new_list = []
 # Without list comprehensions:
 # new_list = []
 # for item in a_list:
 #     new_list.append(item)
 # print(new_list)
 # With list comprehensions:
-# new_list = [new_item for item in a_list]
+# new_list = [item for item in a_list]
+# print(new_list)
+
+# Another example:
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# new_list = []
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+# print(new_list)
+
+# With list comprehensions:
+# new_list = [n + 1 for n in numbers]
 # print(new_list)
