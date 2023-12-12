4693e672bc760fb8c776467e154219f5dc2caefa
Closdlgdo <cidstp031921@gmail.com>
2023-12-12 05:04:32 -0700

    We are learning about list comprehension, with Angela Yu in 100DOP

--- Intermediate/Day26/conditional_lc.py
+++ Intermediate/Day26/conditional_lc.py
@@ -2,4 +2,6 @@
 # Conditional List Comprehensions
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) < 5]
+long_names = [name.upper() for name in names if len(name) > 5]
 print(short_names)
+print(long_names)
