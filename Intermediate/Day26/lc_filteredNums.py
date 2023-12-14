3c79f60c645629c52b25b4765a78f7e130e8f5f8
Closdlgdo <cidstp031921@gmail.com>
2023-12-14 04:42:53 -0700

    We are learning about list comprehension, with Angela Yu in 100DOP

---
+++ Intermediate/Day26/lc_filteredNums.py
@@ -0,0 +1,5 @@
+# We will filter out the even numbers from a series of numbers.
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+filtered_nums = [num for num in nums if num % 2 == 1]
+print(filtered_nums)
