a71133c123575a25e15d2b4ffc9cc53355b9ea5d
Closdlgdo <cidstp031921@gmail.com>
2023-12-17 20:28:04 -0700

    We are working with dictionary comprehension, 100DOP with Dr. Angela Yu.

--- Intermediate/Day26/dictionary_comprehension.py
+++ Intermediate/Day26/dictionary_comprehension.py
@@ -6,14 +6,9 @@
 
 # We can also create a new dictionary with a conditional.
 # new_dict = {new_key:new_value for (key, value) in dict.items() if test}
+import random
+
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-student_scores = {
-    "Alex": 99,
-    "Beth": 89,
-    "Caroline": 99,
-    "Dave": 78,
-    "Eleanor": 99,
-    "Freddie": 89
-}
-passed_students = {name: score for (name, score) in student_scores.items() if score > 90}
-print(passed_students)
+
+student_scores = {student: random.randint(1, 100) for student in names}
+print(student_scores)
