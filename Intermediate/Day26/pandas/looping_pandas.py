1c77b586478a34a42b3224fdd2655442b630e271
Closdlgdo <cidstp031921@gmail.com>
2023-12-21 03:51:58 -0700

    We are working with dictionary comprehension, 100DOP with Dr. Angela Yu.

--- Intermediate/Day26/pandas/looping_pandas.py
+++ Intermediate/Day26/pandas/looping_pandas.py
@@ -13,6 +13,10 @@ import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 # print(student_data_frame)
 
-for (key, value) in student_data_frame.items():
-    print(key)
-    print(value)
+# for (key, value) in student_data_frame.items():
+#     print(key)
+#     print(value)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(row)
