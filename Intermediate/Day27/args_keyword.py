e25873bd89f6d0056a111d4a4d2fb320fc696ce2
Closdlgdo <cidstp031921@gmail.com>
2023-12-24 05:25:21 -0700

    We are working with TKinter creating guis, 100DOP with Dr. Angela Yu.

---
+++ Intermediate/Day27/args_keyword.py
@@ -0,0 +1,15 @@
+# *args - A list of non-keyword arguments to pass to a function. This is a tuple.
+# Limited arguments.
+# def add(n1, n2):
+#     return n1 + n2
+# add(n1=5, n2=3)
+# This only allows you to pass two arguments.
+
+# Unlimited arguments.
+# You dont have to use the word args but you do need the asterisk.
+def add(*args):
+    for n in args:
+        print(n)
+
+
+add(1, 2, 3, 4, 5)
