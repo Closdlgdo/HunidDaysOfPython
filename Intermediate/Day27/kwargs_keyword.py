9371b06feb1bc9ebaa77ee536c2f61436f638bc9
Closdlgdo <cidstp031921@gmail.com>
2023-12-25 04:46:52 -0700

    We are working with TKinter creating guis, 100DOP with Dr. Angela Yu.

--- Intermediate/Day27/kwargs_keyword.py
+++ Intermediate/Day27/kwargs_keyword.py
@@ -1,10 +1,13 @@
 # **kwargs are used to override the default parameters of a function. It is used to pass a dictionary of arguments.
 # We create unlimited key word arguments.
-def calculate(**kwargs):
+def calculate(n, **kwargs):
     print(kwargs)
-    for key, value in kwargs.items():
-        print(key)
-        print(value)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
 
 
-calculate(add=3, multiply=5)
+calculate(2, add=3, multiply=5)
