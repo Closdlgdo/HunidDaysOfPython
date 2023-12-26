100a6e930cd1d5ea91e7da080281b959f71f92da
Closdlgdo <cidstp031921@gmail.com>
2023-12-26 06:09:48 -0700

    We are working with TKinter creating guis, 100DOP with Dr. Angela Yu.

--- Intermediate/Day27/main.py
+++ Intermediate/Day27/main.py
@@ -1,12 +1,24 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Carlos' GUI")
 window.minsize(width=500, height=300)
 
 # Create a label
-my_label = tkinter.Label(text="I am a label", font=("times new roman", 24))
+my_label = Label(text="I am a label", font=("times new roman", 24))
 my_label.pack()  # pack is used to show the label on the screen instead of the console and center it.
+
+# buttons
+button = Button(text="Click me")
+button.pack()
+
+
+
+
+
+
+
+
 
 
 window.mainloop()  # main loop is used to keep the window open
