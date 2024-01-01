dd2d258d6b7bb57bce0e0e831250d362b58c9e58
Closdlgdo <cidstp031921@gmail.com>
2024-01-01 05:31:30 -0700

    We are working with TKinter creating a password manager/creator gui, 100DOP with Dr. Angela Yu.

--- Intermediate/Day29/password manager/main.py
+++ Intermediate/Day29/password manager/main.py
@@ -13,6 +13,28 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1)
+website_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2)
+email_entry.insert(0, "Hello@example.com")
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
 
 window.mainloop()
