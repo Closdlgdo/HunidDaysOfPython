b25c3af960dd41769e6215f5c0280cbad9943704
Closdlgdo <cidstp031921@gmail.com>
2024-01-03 03:55:14 -0700

    We are working with TKinter creating a password manager/creator gui, 100DOP with Dr. Angela Yu.

--- Intermediate/Day29/password manager/main.py
+++ Intermediate/Day29/password manager/main.py
@@ -4,10 +4,11 @@ from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-letters = ['a'', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v
-           ', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q
-           ', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-           numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+           'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 nr_letters = random.randint(8, 10)
@@ -30,6 +31,9 @@ random.shuffle(password_list)
 password = ""
 for char in password_list:
     password += char
+
+print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
