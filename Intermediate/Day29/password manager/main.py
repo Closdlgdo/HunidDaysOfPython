1b85794be654ceb9253722b059a842cc29c57dd9
Closdlgdo <cidstp031921@gmail.com>
2024-01-03 03:59:50 -0700

    We are working with TKinter creating a password manager/creator gui, 100DOP with Dr. Angela Yu.

--- Intermediate/Day29/password manager/main.py
+++ Intermediate/Day29/password manager/main.py
@@ -1,4 +1,4 @@
-import random
+from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox
 
@@ -11,22 +11,13 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+password_letters = [choice(letters) for _ in range(randint(8, 10))]
+password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-password_list = []
+password_list = password_letters + password_symbols + password_numbers
 
-for char in range(nr_letters):
-    password_list.append(random.choice(letters))
-
-for char in range(nr_symbols):
-    password_list += random.choice(symbols)
-
-for char in range(nr_numbers):
-    password_list += random.choice(numbers)
-
-random.shuffle(password_list)
+shuffle(password_list)
 
 password = ""
 for char in password_list:
