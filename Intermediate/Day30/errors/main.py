8588458c39466cbea29984a3f43be4264d27e050
Closdlgdo <cidstp031921@gmail.com>
2024-01-04 04:49:54 -0700

    We are working with TKinter creating a password manager/creator gui, 100DOP with Dr. Angela Yu.

--- Intermediate/Day30/errors/main.py
+++ Intermediate/Day30/errors/main.py
@@ -1,6 +1,21 @@
-with open("a_file.txt") as file:
-    file.read()
+# with open("a_file.txt") as file:
+#     file.read()
+#
+# #  File "/Users/carlosdelgado/Desktop/HunidDaysOfPython/Intermediate/Day30/errors/main.py", line 1, in <module>
+# #     with open("a_file.txt") as file:
+# #  FileNotFoundError: [Errno 2] No such file or directory: 'a_file.txt'
 
-#  File "/Users/carlosdelgado/Desktop/HunidDaysOfPython/Intermediate/Day30/errors/main.py", line 1, in <module>
-#     with open("a_file.txt") as file:
-#  FileNotFoundError: [Errno 2] No such file or directory: 'a_file.txt'
+# # KeyError:
+# a_dict = {"key":"value"}
+# value = a_dict["non_existent_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
+
+# ZeroDivisionError
+# print(5/0)
