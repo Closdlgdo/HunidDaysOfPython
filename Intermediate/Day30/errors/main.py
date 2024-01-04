82713a92eafcff52ad1a2ca4c3bd070fba6b8f9e
Closdlgdo <cidstp031921@gmail.com>
2024-01-04 04:34:02 -0700

    We are working with TKinter creating a password manager/creator gui, 100DOP with Dr. Angela Yu.

---
+++ Intermediate/Day30/errors/main.py
@@ -0,0 +1,6 @@
+with open("a_file.txt") as file:
+    file.read()
+
+#  File "/Users/carlosdelgado/Desktop/HunidDaysOfPython/Intermediate/Day30/errors/main.py", line 1, in <module>
+#     with open("a_file.txt") as file:
+#  FileNotFoundError: [Errno 2] No such file or directory: 'a_file.txt'
