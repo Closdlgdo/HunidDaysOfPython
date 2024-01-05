90edea143a749de78f2843a55f0237a86572c1dc
Closdlgdo <cidstp031921@gmail.com>
2024-01-05 03:42:46 -0700

    We are working with Errors, Exceptions and Json Data, 100DOP with Dr. Angela Yu.

---
+++ Intermediate/Day30/errors/catching_exceptions.py
@@ -0,0 +1,5 @@
+# This is how catching exceptions looks like:
+# try: something that might cause an exception
+# except: do this if there was an exception
+# else: do this if there was no exception
+# finally: do this no matter what
