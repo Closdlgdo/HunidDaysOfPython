4fb47775596d57566005082357ca2c5bb21be34c
Closdlgdo <cidstp031921@gmail.com>
2024-01-05 03:44:15 -0700

    We are working with Errors, Exceptions and Json Data, 100DOP with Dr. Angela Yu.

--- Intermediate/Day30/errors/catching_exceptions.py
+++ Intermediate/Day30/errors/catching_exceptions.py
@@ -3,3 +3,7 @@
 # except: do this if there was an exception
 # else: do this if there was no exception
 # finally: do this no matter what
+try:
+    file = open("a_file.txt")
+except:
+    print("There was an error")
