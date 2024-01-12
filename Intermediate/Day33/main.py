b5f773add93570e2a1d9e7c42cb0e85355b656a6
Closdlgdo <cidstp031921@gmail.com>
2024-01-12 04:02:25 -0700

    We are working with APIS, 100DOP with Dr. Angela Yu.

--- Intermediate/Day33/main.py
+++ Intermediate/Day33/main.py
@@ -3,3 +3,7 @@
 # An API Endpoint is the website of the application. Like api.example.com.
 # The API Request is when we are asking the website for information. We cant just take money from
 # a bank safe, we need to make a request to the teller. That is an api request.
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response.status_code)
