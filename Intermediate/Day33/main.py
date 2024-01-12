234b972bee46be4786c6fafa720993dd332c4ab4
Closdlgdo <cidstp031921@gmail.com>
2024-01-12 03:47:04 -0700

    We are working with APIS, 100DOP with Dr. Angela Yu.

---
+++ Intermediate/Day33/main.py
@@ -0,0 +1,5 @@
+# An Application Programming Interface (API) is a set of commands, functions, protocols and objects that programmers use to create
+# software or interact with an external system.
+# An API Endpoint is the website of the application. Like api.example.com.
+# The API Request is when we are asking the website for information. We cant just take money from
+# a bank safe, we need to make a request to the teller. That is an api request.
